8310f8796a1e79725aa9960c9a58275a552f203d
Will Brown <wtbrown10@gmail.com>
2020-04-09 19:24:11 -0400

    added 2 arrays

---
+++ nestedarrays.js
@@ -0,0 +1,6 @@
+let candyType = ["Fruity", "Chocolatey", "Peanut Butter"];
+let candyNames = [
+  ["fruit Roll up", "Life Savers", "Skittles"],
+  ["Hershey", "Cadbury", "Milky Way"],
+  ["Reeses", "ButterFingers", "MrGood Bar"],
+];
